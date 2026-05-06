ae845a58d028c75d08acc50efcee079544cdaedb
Baxtiyar Mammadyarov <74107721+BaxtiyarMammadyarov@users.noreply.github.com>
2026-05-06 17:17:13 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 // ─── Layihə məlumatları ───────────────────────────────────────────────────────
 group   = "az.mbm"
-version = "1.0.10"
+version = "1.1.0"
 
 // ─── Java versiyası ───────────────────────────────────────────────────────────
 java {
@@ -66,7 +66,7 @@ publishing {
 
             groupId    = "az.mbm"
             artifactId = "jooq-sql-generate"
-            version    = "1.0.10"
+            version    = "1.1.0"
 
             pom {
                 name        = "jooq-sql-generate"
